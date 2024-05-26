575c1f250b3e9c35fc3b1a706c64d1b0636b0fc2
Nitza.pvz <L20170859@culiacan.tecnm.mx>
2024-05-26 10:24:50 -0700

    Optimización y validacines de la clase Main: Pasos del cajero

--- src/Main.java
+++ src/Main.java
@@ -30,6 +30,7 @@ public class Main {
     static ArrayList<Cliente> clientes;
     static ArrayList<Cuenta> cuentas;
     static ArrayList<TarjetaDebito> tarjetas;
+    static boolean estado = true;
 
     /*PASOS DEL CAJERO AUTOMATICO
             1. Ingreso del numero de cuenta.
@@ -39,69 +40,104 @@ public class Main {
                 3.1. Retira el dinero en efectivo
                 3.2. Ingresa la cuenta destino e inicia transferencia.
             4. Imprime ticket
-            5. Finaliza
+            5. Finaliza y regresa al paso 1.
     */
-
     public static void main(String[] args) {
         //DATOS PRECARGADOS
         init();
-        
-        //PANTALLA INICIAL DEL CAJERO
-        System.out.println("-----------------------------------------");
-        System.out.println("Bienvenido al cajero automático "+atm.banco);
-        //Ingreso del número de cuenta
-        System.out.println("Ingrese su número de cuenta: ");
-        String numCuenta = entrada.nextLine();
-    //Ingreso del NIP para acceso a la cuenta
-    //System.out.println("Ingrese su NIP: ");
-    //int nip = entrada.nextInt();
+        String numCuenta;
+        /*  Ciclo que mantiene actividad en el cajero.Se mantendrá
+            activo y repetitivo para el siguiente cliente. Dado que
+            el cliente cometa algun error, simplemente pasa a la
+            siguiente iteracion del ciclo.         
+         */
+        do {
+            //PANTALLA INICIAL DEL CAJERO
+            System.out.println("-----------------------------------------");
+            System.out.println("Bienvenido al cajero automático "+atm.banco);
+            //Ingreso del número de cuenta
+            System.out.println("Ingrese su número de cuenta: ");
+            numCuenta = entrada.next();
+            //Selección del tipo de cuenta
+            System.out.println("Seleccione el tipo de cuenta: ");
+            System.out.println("1. Cuenta de Ahorro");
+            System.out.println("2. Cuenta de Cheques");
+            int opTipo = 0;
+            String tipoCta = "";
+            /*Ciclo para que solo tenga las dos opciones que se presentan.
+             *Segun la opcion elegida, la almacena en un String para buscar
+                si existe.
+            */
+            do {
+                opTipo = entrada.nextInt();
+                if (opTipo == 1)
+                    tipoCta = "Cuenta de Ahorro";
+                if (opTipo == 2)
+                    tipoCta = "Cuenta de Cheques";
+            } while (opTipo != 1 && opTipo != 2);
+        //Ingreso del NIP para acceso a la cuenta
+        //System.out.println("Ingrese su NIP: ");
+        //int nip = entrada.nextInt();
 
-        /* DETERMINAR SI LA CUENTA EXISTE
-                Instancia de la clase Cuenta que llama al método
-                buscarCuenta(Cuenta), para verificar si existe.
-                Muestra el nombre y saldo disponible del cliente.
-        */
-        Cuenta cuentaOr = buscarCuenta(numCuenta);
-        System.out.println("-----------------------------------------");
-        System.out.println("Bienvenido "+cuentaOr.cliente.nombre);
-        System.out.println("SALDO: $"+cuentaOr.saldo);
+            /* DETERMINAR SI LA CUENTA EXISTE
+                    Instancia de la clase Cuenta que llama al método
+                    buscarCuenta(Cuenta), para verificar si existe.
+                    Si existe, muestra el nombre y saldo disponible del cliente.
+                    Sino, indica que no existe al cliente y rompe la iteracion
+                    del ciclo para dar lugar a otra entrada de datos al cliente.
+            */
+            Cuenta cuentaOr = buscarCuenta(numCuenta);
+            if (cuentaOr == null) {
+                System.out.println("Su cuenta no existe\nPorfavor intentelo de nuevo\n\n");
+                continue;
+            }
+            System.out.println("-----------------------------------------");
+            System.out.println("Bienvenido "+cuentaOr.cliente.nombre);
+            System.out.println("SALDO: $"+cuentaOr.saldo);
 
-        /*  SELECCION DEL TIPO DE TRANSACCION
-                Dado que en ambas transacciones debe ingresar un monto,
-                se ingresa primero antes de llamar al método.
-                    1. Transferencia. Ingresa la cuenta destino y el ciclo
-                    determina si existe, para llamar al método Tranferir de
-                    Cajero. En caso contrario, cancela el proceso.
-                    2. Retiro. Simplemente llama al método de Retirar de la
-                    clase Cajero.
-                    ?. Cancela el proceso.
-        */
-        System.out.println("Seleccione el tipo de transacción:");
-        System.out.println("1. Transferencia");
-        System.out.println("2. Retiro de Efectivo");
-        int op = entrada.nextInt();
-        //Ingreso del monto
-        System.out.print("Ingrese el monto: ");
-        double monto = entrada.nextDouble();
-        if (op == 1) {
-            System.out.println("Ingrese el número de cuenta de destino: ");
-            String cuentaDestino = entrada.next();
-            Cuenta ctaDestino = buscarCuenta(cuentaDestino);
-            //System.out.println("Saldo antes: "+ctaDestino.saldo);
-            atm.Transferir(cuentaOr, ctaDestino, ctaDestino.tipo, monto);
-            //System.out.println("Saldo despues: "+ctaDestino.saldo);
-        } else if (op == 2)  {
-            atm.Retirar(cuentaOr, cuentaOr.tipo, monto);
-        } else {
-            System.out.println("Opción no válida... Saliendo");
-        }
+            /*  SELECCION DEL TIPO DE TRANSACCION
+                    Dado que en ambas transacciones debe ingresar un monto,
+                    se ingresa primero antes de llamar al método.
+                        1. Transferencia. Ingresa la cuenta destino y el ciclo
+                        determina si existe, para llamar al método Tranferir de
+                        Cajero. En caso contrario, cancela el proceso.
+                        2. Retiro. Simplemente llama al método de Retirar de la
+                        clase Cajero.
+                        ?. Cancela el proceso.
+            */
+            System.out.println("Seleccione el tipo de transacción:");
+            System.out.println("1. Transferencia");
+            System.out.println("2. Retiro de Efectivo");
+            int op = entrada.nextInt();
+            if (op != 1 && op != 2) {
+                System.out.println("\nOpción no válida... Saliendo\n\n");
+                continue;
+            }
+            //Ingreso del monto
+            System.out.print("Ingrese el monto: ");
+            double monto = entrada.nextDouble();
+            if (op == 1) {
+                System.out.println("Ingrese el número de cuenta de destino: ");
+                String cuentaDestino = entrada.next();
+                Cuenta ctaDestino = buscarCuenta(cuentaDestino);
+                if (ctaDestino == null) {
+                    System.out.println("La cuenta no existe... Saliendo\n\n");
+                    continue;
+                }
+                //System.out.println("Saldo antes: "+ctaDestino.saldo);
+                atm.Transferir(cuentaOr, ctaDestino, ctaDestino.tipo, monto);
+                //System.out.println("Saldo despues: "+ctaDestino.saldo);
+            }
+            if (op == 2)  {
+                atm.Retirar(cuentaOr, cuentaOr.tipo, monto);
+            }
+        } while (estado);
         entrada.close();
     }
 
     public static Cuenta buscarCuenta(String numCuenta) {
         /*Método que verifica si la cuenta existe.
-         *Retorna la instancia de la cuenta.
-         */
+         *Retorna la instancia de la cuenta. */
         Cuenta cuentaOr = null;
         for (Cuenta cuenta : cuentas) {
             if (cuenta.numeroCuenta.equals(numCuenta)) {
@@ -126,8 +162,8 @@ public class Main {
         cuentas = new ArrayList<>();
         Cuenta cta01 = new Cuenta("Cuenta de Ahorro", cte01, "9876543210", 2500);
         Cuenta cta02 = new Cuenta("Cuenta de Cheques", cte02, "1234567890", 3200);
-        Cuenta cta03 = new Cuenta("Cuenta de Ahorro", cte03, "6549873210", 55);
-        Cuenta cta04 = new Cuenta("Cuenta de Cheques", cte01, "9638527410", 125);
+        Cuenta cta03 = new Cuenta("Cuenta de Ahorro", cte03, "6549873210", 505);
+        Cuenta cta04 = new Cuenta("Cuenta de Cheques", cte01, "9876543210", 125);
         cuentas.add(cta01); cuentas.add(cta02); cuentas.add(cta03); cuentas.add(cta04);
         //Tarjetas
         tarjetas = new ArrayList<>();
